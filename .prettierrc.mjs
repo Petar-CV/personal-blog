11f0f8a3db2ee7ecb9049bb9992156f3c2325273
Petar Cvetko Voćanec <cvetkovocanecpetar@gmail.com>
2023-11-01 13:27:26 +0100

    Removed unsupported import order Prettier plugin

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,13 +1,11 @@
 // .prettierrc.mjs
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ['prettier-plugin-astro', '@trivago/prettier-plugin-sort-imports'],
+  plugins: ['prettier-plugin-astro'],
   semi: true,
   tabWidth: 2,
   singleQuote: true,
   jsxSingleQuote: true,
-  importOrder: ['<THIRD_PARTY_MODULES>', '^@/(.*)$', '^[./]'],
-  importOrderSeparation: true,
   overrides: [
     {
       files: '*.astro',
