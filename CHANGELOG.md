697b9e8029e33b84d9608a98541a35db58f37f04
Petar Cvetko Voćanec <cvetkovocanecpetar@gmail.com>
2023-11-01 14:07:55 +0100

    Added TailwindCSS and replaced various styles with Tailwind classes

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## 0.0.1 (01-11-2023)
+
+- Initialized Astro project with blog template
+- Setup TailwindCSS
+- Setup Prettier plugin
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), tailwind()],
 });
