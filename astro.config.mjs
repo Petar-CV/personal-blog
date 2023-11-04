877e7eaa8ca5370b5c9c6936b0169d40b5cbf374
Petar Cvetko Voćanec <cvetkovocanecpetar@gmail.com>
2023-11-04 14:43:24 +0100

    Added production site to Astro config so Astro can generate sitemap and canonical URLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://blog.petar-cv.com',
   integrations: [mdx(), sitemap(), tailwind()],
 });
