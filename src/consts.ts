9a38f60e110fcff29d5feec8044964011383a1bd
Petar Cvetko Voćanec <cvetkovocanecpetar@gmail.com>
2023-11-05 13:11:04 +0100

    Improved header responsiveness for mobile devices, closes #19

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Dev Blog by Petar CV';
+export const SHORT_SITE_TITLE = 'Dev Blog';
 export const SITE_DESCRIPTION =
   'Personal developer blog by Petar Cvetko Voćanec';
