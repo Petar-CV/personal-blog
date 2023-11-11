81ae94f9ccb193c01124a8c648a1cbe02ac8dfbd
Petar Cvetko Voćanec <cvetkovocanecpetar@gmail.com>
2023-11-11 10:32:10 +0100

    Headings from markdown are now automatically turned into links

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,17 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import { rehypeHeadingIds } from '@astrojs/markdown-remark';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.petar-cv.com',
+  markdown: {
+    rehypePlugins: [
+      rehypeHeadingIds,
+      [rehypeAutolinkHeadings, { behavior: 'wrap' }],
+    ],
+  },
   integrations: [mdx(), sitemap(), tailwind()],
 });
